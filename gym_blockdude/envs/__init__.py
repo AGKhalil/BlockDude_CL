a169b3ed62a5665059ed05f87cef7d55c3f21e8c
AGKhalil <elghazalykhalil@gmail.com>
2019-05-08 18:51:20 +0100

    spelling

--- gym_blockdude/envs/__init__.py
+++ gym_blockdude/envs/__init__.py
@@ -1,4 +1,4 @@
 from gym_blockdude.envs.blockdude_env import BlockDude
-from gym_blockdude.envs.blockdude_medium_env import BlockDudeMed
+from gym_blockdude.envs.blockdude_med_env import BlockDudeMed
 from gym_blockdude.envs.blockdude_hard_env import BlockDudeHard
 from gym_blockdude.envs.blockdude_extrahard_env import BlockDudeExtraHard
